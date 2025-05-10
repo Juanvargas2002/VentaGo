37dd0fcb6bbb15e5b569b17ccf6da321edd4e4f7
Jvargas <juan20028c@gmail.com>
2025-05-10 10:39:29 -0500

    se generan opciones desplegables para toda la sidebar

--- vistas/contenidos/categorias-view.php
+++ vistas/contenidos/categorias-view.php
@@ -12,13 +12,6 @@
     <div class="d-flex align-items-center mb-4">
         <h1 class="h3 text-gray-800 mr-3">Categorías</h1>
 
-        <!-- Añadir categoría -->
-        <a href="<?php echo SERVER_URL; ?>categoria-new/" class="btn btn-primary btn-icon-split">
-            <span class="icon text-white-50">
-                <i class="fas fa-plus"></i>
-            </span>
-            <span class="text">Añadir</span>
-        </a>
     </div>
 
     <!-- Filtro de Categorías -->
